e7bef04403892792d45d1aa9deec5f9e587764ab
katepang13 <katepangliu@gmail.com>
2021-07-01 22:20:14 +0800

    geometry material animate

--- Graphics/Threejs/threejs-scroll-animation-demo/vite-project/main.js
+++ Graphics/Threejs/threejs-scroll-animation-demo/vite-project/main.js
@@ -15,4 +15,20 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize( window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
-renderer.render();
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const meterial = new THREE.MeshBasicMaterial( {color: 0xFF6347, wireframe: true} );
+const torus = new THREE.Mesh( geometry, meterial);
+
+scene.add(torus);
+
+function animate() {
+  requestAnimationFrame( animate );
+
+  torus.rotateX(0.01)
+  torus.rotateY(0.01)
+  torus.rotateZ(0.01)
+
+  renderer.render(scene, camera);
+}
+
+animate()
