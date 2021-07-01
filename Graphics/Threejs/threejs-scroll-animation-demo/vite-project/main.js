b725a90189e0f2d4d4003c940a486500419bd354
katepang13 <katepangliu@gmail.com>
2021-07-01 22:36:54 +0800

    Random Generation

--- Graphics/Threejs/threejs-scroll-animation-demo/vite-project/main.js
+++ Graphics/Threejs/threejs-scroll-animation-demo/vite-project/main.js
@@ -35,6 +35,21 @@ scene.add(lightHelper, gridHelper);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material =  new THREE.MeshStandardMaterial({color: 0xffffff})
+  const star = new THREE.Mesh( geometry, material );
+
+  const [x, y ,z] = Array(3).fill().map(()=> 
+    THREE.MathUtils.randFloatSpread( 100 )
+  );
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
 function animate() {
   requestAnimationFrame( animate );
 
