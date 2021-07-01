a75b7c44cdcfafc24d8b2929de14b6346cc16032
katepang13 <katepangliu@gmail.com>
2021-07-01 22:39:33 +0800

    Scene Background

--- Graphics/Threejs/threejs-scroll-animation-demo/vite-project/main.js
+++ Graphics/Threejs/threejs-scroll-animation-demo/vite-project/main.js
@@ -50,6 +50,9 @@ function addStar() {
 
 Array(200).fill().forEach(addStar);
 
+const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+scene.background = spaceTexture;
+
 function animate() {
   requestAnimationFrame( animate );
 
